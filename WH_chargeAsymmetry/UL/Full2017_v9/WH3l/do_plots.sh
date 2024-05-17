acdc461413f56915dbcaa4cec840ae7d63e27398
ntrevisa <nicolo.trevisani@cern.ch>
2024-05-17 09:40:12 +0200

    plots WH3l 2017

--- WH_chargeAsymmetry/UL/Full2017_v9/WH3l/do_plots.sh
+++ WH_chargeAsymmetry/UL/Full2017_v9/WH3l/do_plots.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]; then
+	echo "No arguments passed. Plots will not be copied on webpage"
+else
+	echo "We got some argument(s)"
+	echo "==========================="
+	echo "Number of arguments. : $#"
+	echo "List of arguments... : $@"
+	echo "Arg #1: date         : $1"
+	echo "==========================="
+	DATE=$1
+fi
+
 cd configuration_OSSF
 mkShapesRDF -c 1
 mkPlot --onlyPlot cratio --showIntegralLegend 1 --fileFormats png
@@ -26,3 +38,9 @@ mkPlot --onlyPlot cratio --showIntegralLegend 1 --fileFormats png
 cd -
 
 cp ~/index.php plots_WH3l_2017_v9_chargeAsymmetry_Mu82_EleUL90/
+
+if [ "$DATE" != "" ]; then
+	echo "Copying plots to the web ..."
+	cp -r plots_WH3l_2017_v9_chargeAsymmetry_Mu82_EleUL90/ /eos/user/n/ntrevisa/www/plots/${DATE}/2017/
+	echo "Done!"
+fi
