5064ff6bb4683974f52f42fb2092a1b785b5fd28
ntrevisa <nicolo.trevisani@cern.ch>
2024-06-25 13:23:24 +0200

    first cfg to run on new production

---
+++ ControlRegions/DY/2022EE_v12/samples.py
@@ -0,0 +1,173 @@
+from mkShapesRDF.lib.search_files import SearchFiles
+
+searchFiles = SearchFiles()
+
+redirector = ""
+useXROOTD = False
+
+# #only data 2022
+# mcProduction = 'Summer22_130x_nAODv12_Full2022v12'
+# dataReco = 'Run2022_ReReco_nAODv12_Full2022v12'
+# mcSteps = 'MCl1loose2022v12__MCCorr2022v12_NoJER'
+# fakeSteps = 'DATAl1loose2022v12__fakeW'
+# dataSteps = 'DATAl1loose2022v12__fakeW'
+
+mcProduction = 'Summer22EE_130x_nAODv12_Full2022v12'
+mcSteps      = 'MCl2loose2022EEv12__MCCorr2022EEv12__l2tight'
+dataReco     = 'Run2022EE_Prompt_nAODv12_Full2022v12'
+# fakeSteps    = 'DATAl1loose2022EFGv12__fakeW'
+dataSteps    = 'DATAl2loose2022EEv12__l2tight'
+
+##############################################
+###### Tree base directory for the site ######
+##############################################
+treeBaseDir = '/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano'
+limitFiles = -1
+
+def makeMCDirectory(var=""):
+    _treeBaseDir = treeBaseDir + ""
+    if redirector != "":
+        _treeBaseDir = redirector + treeBaseDir
+    if var == "":
+        return "/".join([_treeBaseDir, mcProduction, mcSteps])
+    else:
+        return "/".join([_treeBaseDir, mcProduction, mcSteps + "__" + var])
+
+
+mcDirectory   = makeMCDirectory()
+# fakeDirectory = os.path.join(treeBaseDir, dataReco, fakeSteps)
+dataDirectory = os.path.join(treeBaseDir, dataReco, dataSteps)
+
+samples = {}
+
+
+def nanoGetSampleFiles(path, name):
+    _files = searchFiles.searchFiles(path, name, redirector=redirector)
+    if limitFiles != -1 and len(_files) > limitFiles:
+        return [(name, _files[:limitFiles])]
+    else:
+        return [(name, _files)]
+
+
+def CombineBaseW(samples, proc, samplelist):
+    _filtFiles = list(filter(lambda k: k[0] in samplelist, samples[proc]["name"]))
+    _files = list(map(lambda k: k[1], _filtFiles))
+    _l = list(map(lambda k: len(k), _files))
+    leastFiles = _files[_l.index(min(_l))]
+    dfSmall = ROOT.RDataFrame("Runs", leastFiles)
+    s = dfSmall.Sum("genEventSumw").GetValue()
+    f = ROOT.TFile(leastFiles[0])
+    t = f.Get("Events")
+    t.GetEntry(1)
+    xs = t.baseW * s
+
+    __files = []
+    for f in _files:
+        __files += f
+    df = ROOT.RDataFrame("Runs", __files)
+    s = df.Sum("genEventSumw").GetValue()
+    newbaseW = str(xs / s)
+    weight = newbaseW + "/baseW"
+
+    for iSample in samplelist:
+        addSampleWeight(samples, proc, iSample, weight)
+
+
+def addSampleWeight(samples, sampleName, sampleNameType, weight):
+    obj = list(filter(lambda k: k[0] == sampleNameType, samples[sampleName]["name"]))[0]
+    samples[sampleName]["name"] = list(
+        filter(lambda k: k[0] != sampleNameType, samples[sampleName]["name"])
+    )
+    if len(obj) > 2:
+        samples[sampleName]["name"].append(
+            (obj[0], obj[1], obj[2] + "*(" + weight + ")")
+        )
+    else:
+        samples[sampleName]["name"].append((obj[0], obj[1], "(" + weight + ")"))
+
+
+################################################
+############ DATA DECLARATION ##################
+################################################
+
+DataRun = [
+    ['B','Run2022B-ReReco-v1'],
+    ['C','Run2022C-ReReco-v1'],
+    ['D','Run2022D-ReReco-v1'],
+    ['E','Run2022E-Prompt-v1'],
+    ['F','Run2022F-Prompt-v1'],
+    ['G','Run2022G-Prompt-v1'],
+]
+
+DataSets = ['MuonEG','SingleMuon','Muon','EGamma']
+
+DataTrig = {
+    'MuonEG'         : ' Trigger_ElMu' ,
+    'SingleMuon'     : '!Trigger_ElMu && Trigger_sngMu' ,
+    'Muon'           : '!Trigger_ElMu && (Trigger_sngMu || Trigger_dblMu)',
+    'EGamma'         : '!Trigger_ElMu && !Trigger_sngMu && !Trigger_dblMu && (Trigger_sngEl || Trigger_dblEl)'
+}
+
+
+#########################################
+############ MC COMMON ##################
+#########################################
+
+# SFweight does not include btag weights
+mcCommonWeightNoMatch = 'XSWeight*METFilter_Common*SFweight'
+mcCommonWeight        = 'XSWeight*METFilter_Common*PromptGenLepMatch2l*SFweight'
+
+#mcCommonWeight = 'XSWeight*METFilter_Common*SFweight'
+
+###########################################
+#############  BACKGROUNDS  ###############
+###########################################
+
+###### DY #######
+
+files = nanoGetSampleFiles(mcDirectory, 'DYto2L-2Jets_MLL-50')
+    # nanoGetSampleFiles(mcDirectory, 'DYto2L-2Jets_MLL-10to50')
+
+
+samples['DY'] = {
+    'name': files,
+    #'weight': mcCommonWeight + '*8.174732641*( !(Sum(PhotonGen_isPrompt==1 && PhotonGen_pt>15 && abs(PhotonGen_eta)<2.6) > 0))',
+    'weight': mcCommonWeight,
+    'FilesPerJob': 2,
+    'subsamples': {
+        'nHardJets_0' : 'nHardJets == 0',
+        'nHardJets_1' : 'nHardJets == 1',
+        'nHardJets_2' : 'nHardJets >= 2',
+    }
+
+}
+
+###########################################
+################## DATA ###################
+###########################################
+
+samples['DATA'] = { 
+    'name': [],  
+    'weight': 'LepWPCut*METFilter_DATA',     
+    'weights': [], 
+    'isData': ['all'], 
+    'FilesPerJob': 15 
+} 
+
+for _, sd in DataRun:
+  for pd in DataSets:
+    datatag = pd + '_' + sd
+
+    if (pd == "SingleMuon" and _ in ["D","E","F","G"]) or (pd == "Muon" and _ == "B"):
+        continue
+    
+    print(datatag)
+
+    if (_ in ["E","F","G"]):
+        files = nanoGetSampleFiles(dataDirectory, datatag)
+    else:
+        continue
+        # files = nanoGetSampleFiles(dataDirectory, datatag)
+
+    samples['DATA']['name'].extend(files)
+    addSampleWeight(samples, 'DATA', datatag, DataTrig[pd])
