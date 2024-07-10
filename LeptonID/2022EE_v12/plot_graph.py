d9216460fd1ab14894f3c5ffdc923434ecf64d78
ntrevisa <nicolo.trevisani@cern.ch>
2024-07-10 11:45:01 +0200

    also muon tthmva plots

--- LeptonID/2022EE_v12/plot_graph.py
+++ LeptonID/2022EE_v12/plot_graph.py
@@ -1,15 +1,6 @@
 import ROOT
 import sys,os
 
-ele_ids  = ["wp90iso","mvaWinter22V2Iso_WP90"]
-muon_ids = ["cut_TightID_POG","cut_Tight_HWW","cut_TightMiniIso_HWW","mvaMuID_WP_medium","mvaMuID_WP_tight"] 
-
-signals     = ["WW","ggH_hww"]
-backgrounds = ["TTToSemiLeptonic","WJets"]
-
-final_states = ["ee","em","mm"]
-pt_ranges    = ["high_pt","low_pt"]
-
 input_file_name = "eff_plots/efficiencies.root"
 
 colors = [ROOT.kGreen+1,ROOT.kRed+1,ROOT.kBlue,ROOT.kOrange,ROOT.kMagenta,ROOT.kAzure+10]
@@ -38,6 +29,9 @@ def plot_canvas(input_file_name,
 
     # Trick to allow legend to get all the graphs --> put them into a list
     graphs = []
+
+    # Draw a frame with correct axis ranges. Otherwise, TGraphs will choose their own axis ranges.
+    c1.DrawFrame(0.0,0.0,1.0,1.0)
             
     for ele_id in ele_ids:
         for muon_id in muon_ids:
@@ -52,14 +46,14 @@ def plot_canvas(input_file_name,
                     graphs.append(input_file.Get(graph_name))
                     graphs[-1].SetMarkerStyle(20)
                     graphs[-1].SetMarkerColor(colors[first_graph])
-                    graphs[-1].GetXaxis().SetRangeUser(0,1)
-                    graphs[-1].GetYaxis().SetRangeUser(0,1)
-                    if focus == 'muon': graphs[-1].SetName(muon_id)
-                    if focus == 'ele':  graphs[-1].SetName(ele_id)
-                    if focus == 'both': graphs[-1].SetName(ele_id + "_" + muon_id)
-                    graphs[-1].Draw("AP")
-                    graphs[-1].GetXaxis().SetRangeUser(0,1)
-                    graphs[-1].GetYaxis().SetRangeUser(0,1)
+                    graphs[-1].GetXaxis().SetRangeUser(0.0,1.0)
+                    graphs[-1].GetYaxis().SetRangeUser(0.0,1.0)
+                    if focus == 'mm': graphs[-1].SetName(muon_id)
+                    if focus == 'ee': graphs[-1].SetName(ele_id)
+                    if focus == 'em': graphs[-1].SetName(ele_id + "_" + muon_id)
+                    graphs[-1].Draw("P")
+                    graphs[-1].GetXaxis().SetRangeUser(0.0,1.0)
+                    graphs[-1].GetYaxis().SetRangeUser(0.0,1.0)
                     print("First graph plotted!")
                     print(f"Values = ({graphs[-1].GetPointX(0)},{graphs[-1].GetPointY(0)})")
                     first_graph += 1
@@ -70,9 +64,9 @@ def plot_canvas(input_file_name,
                     graphs.append(input_file.Get(graph_name))
                     graphs[-1].SetMarkerStyle(20)
                     graphs[-1].SetMarkerColor(colors[first_graph])
-                    if focus == 'muon': graphs[-1].SetName(muon_id)
-                    if focus == 'ele':  graphs[-1].SetName(ele_id)
-                    if focus == 'both': graphs[-1].SetName(ele_id + "_" + muon_id)
+                    if focus == 'mm': graphs[-1].SetName(muon_id)
+                    if focus == 'ee': graphs[-1].SetName(ele_id)
+                    if focus == 'em': graphs[-1].SetName(ele_id + "_" + muon_id)
                     graphs[-1].Draw("P,same")
                     print(f"Graph {first_graph+1} plotted!")
                     print(f"Values = ({graphs[-1].GetPointX(0)},{graphs[-1].GetPointY(0)})")
@@ -81,9 +75,9 @@ def plot_canvas(input_file_name,
     # Legend                
     leg = ROOT.TLegend(0.12,0.12,0.82,0.42)
     leg.SetLineColor(0)
-    if focus == 'muon':   leg.SetHeader("Muon ID:")
-    elif focus == 'ele':  leg.SetHeader("Electron ID:")
-    elif focus == 'both': leg.SetHeader("Electron and Muon ID:")
+    if focus == 'mm':   leg.SetHeader("Muon ID:")
+    elif focus == 'ee':  leg.SetHeader("Electron ID:")
+    elif focus == 'em': leg.SetHeader("Electron and Muon ID:")
     else:
         raise ValueError("Please spcify what I should put my focus on")
     for g in graphs:
@@ -97,35 +91,53 @@ def plot_canvas(input_file_name,
     
 
 # Calling the function to produce plots 
-plot_canvas(input_file_name,"WW","WJets",ele_ids,muon_ids,"ee","high_pt","ee_WW_vs_Wjets","ele")
-plot_canvas(input_file_name,"WW","WJets",ele_ids,muon_ids,"em","high_pt","em_WW_vs_Wjets","both")
-plot_canvas(input_file_name,"WW","WJets",ele_ids,muon_ids,"mm","high_pt","mm_WW_vs_Wjets","muon")
+ele_ids  = ["wp90iso","mvaWinter22V2Iso_WP90"]
+muon_ids = ["cut_TightID_POG","cut_Tight_HWW","cut_TightMiniIso_HWW","mvaMuID_WP_medium","mvaMuID_WP_tight"] 
 
-plot_canvas(input_file_name,"ggH_hww","WJets",ele_ids,muon_ids,"ee","high_pt","ee_ggH_vs_Wjets","ele")
-plot_canvas(input_file_name,"ggH_hww","WJets",ele_ids,muon_ids,"em","high_pt","em_ggH_vs_Wjets","both")
-plot_canvas(input_file_name,"ggH_hww","WJets",ele_ids,muon_ids,"mm","high_pt","mm_ggH_vs_Wjets","muon")
+signals     = ["WW","ggH_hww"]
+backgrounds = ["TTToSemiLeptonic","WJets","TTToSemiLeptonic,WJets"]
 
-plot_canvas(input_file_name,"WW","TTToSemiLeptonic",ele_ids,muon_ids,"ee","high_pt","ee_WW_vs_Top","ele")
-plot_canvas(input_file_name,"WW","TTToSemiLeptonic",ele_ids,muon_ids,"em","high_pt","em_WW_vs_Top","both")
-plot_canvas(input_file_name,"WW","TTToSemiLeptonic",ele_ids,muon_ids,"mm","high_pt","mm_WW_vs_Top","muon")
-
-plot_canvas(input_file_name,"ggH_hww","TTToSemiLeptonic",ele_ids,muon_ids,"ee","high_pt","ee_ggH_vs_Top","ele")
-plot_canvas(input_file_name,"ggH_hww","TTToSemiLeptonic",ele_ids,muon_ids,"em","high_pt","em_ggH_vs_Top","both")
-plot_canvas(input_file_name,"ggH_hww","TTToSemiLeptonic",ele_ids,muon_ids,"mm","high_pt","mm_ggH_vs_Top","muon")
+final_states = ["ee","em","mm"]
+pt_ranges    = ["high_pt","low_pt"]
 
 
-plot_canvas(input_file_name,"WW","WJets",ele_ids,muon_ids,"ee","low_pt","ee_WW_vs_Wjets_low_pT","ele")
-plot_canvas(input_file_name,"WW","WJets",ele_ids,muon_ids,"em","low_pt","em_WW_vs_Wjets_low_pT","both")
-plot_canvas(input_file_name,"WW","WJets",ele_ids,muon_ids,"mm","low_pt","mm_WW_vs_Wjets_low_pT","muon")
+# Loop over IDs without tthmva
+for signal in signals:
+    for background in backgrounds:
+        for final_state in final_states:
+            for pt_range in pt_ranges:
 
-plot_canvas(input_file_name,"ggH_hww","WJets",ele_ids,muon_ids,"ee","low_pt","ee_ggH_vs_Wjets_low_pT","ele")
-plot_canvas(input_file_name,"ggH_hww","WJets",ele_ids,muon_ids,"em","low_pt","em_ggH_vs_Wjets_low_pT","both")
-plot_canvas(input_file_name,"ggH_hww","WJets",ele_ids,muon_ids,"mm","low_pt","mm_ggH_vs_Wjets_low_pT","muon")
+                tthmva = ""
+                low_pt = ""
+                if "tthmva" in muon_ids[0]:
+                    tthmva = "_tthmva"
+                if pt_range == "low_pt":
+                    low_pt = "_low_pt"
 
-plot_canvas(input_file_name,"WW","TTToSemiLeptonic",ele_ids,muon_ids,"ee","low_pt","ee_WW_vs_Top_low_pT","ele")
-plot_canvas(input_file_name,"WW","TTToSemiLeptonic",ele_ids,muon_ids,"em","low_pt","em_WW_vs_Top_low_pT","both")
-plot_canvas(input_file_name,"WW","TTToSemiLeptonic",ele_ids,muon_ids,"mm","low_pt","mm_WW_vs_Top_low_pT","muon")
+                background = background.replace(",","_")
+                    
+                output_name = f"{final_state}_{signal}_vs_{background}{low_pt}{tthmva}"
+                    
+                plot_canvas(input_file_name,signal,background,ele_ids,muon_ids,final_state,pt_range,output_name,final_state)
 
-plot_canvas(input_file_name,"ggH_hww","TTToSemiLeptonic",ele_ids,muon_ids,"ee","low_pt","ee_ggH_vs_Top_low_pT","ele")
-plot_canvas(input_file_name,"ggH_hww","TTToSemiLeptonic",ele_ids,muon_ids,"em","low_pt","em_ggH_vs_Top_low_pT","both")
-plot_canvas(input_file_name,"ggH_hww","TTToSemiLeptonic",ele_ids,muon_ids,"mm","low_pt","mm_ggH_vs_Top_low_pT","muon")
+# Loop over IDs with tthmva
+ele_ids  = ["wp90iso","mvaWinter22V2Iso_WP90"]
+muon_ids = ["cut_TightID_POG_tthmva_80","cut_Tight_HWW_tthmva_80","cut_TightMiniIso_HWW_tthmva_80","mvaMuID_WP_medium_tthmva_80","mvaMuID_WP_tight_tthmva_80"] 
+
+for signal in signals:
+    for background in backgrounds:
+        for final_state in final_states:
+            for pt_range in pt_ranges:
+
+                tthmva = ""
+                low_pt = ""
+                if "tthmva" in muon_ids[0]:
+                    tthmva = "_tthmva"
+                if pt_range == "low_pt":
+                    low_pt = "_low_pt"
+
+                background = background.replace(",","_")
+
+                output_name = f"{final_state}_{signal}_vs_{background}{low_pt}{tthmva}"
+                    
+                plot_canvas(input_file_name,signal,background,ele_ids,muon_ids,final_state,pt_range,output_name,final_state)
