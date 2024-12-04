4d508e324d393b01b05144605491528a33a03032
ntrevisa <nicolo.trevisani@cern.ch>
2024-12-04 09:35:57 +0100

    First commit of top CR 2022EE

---
+++ ControlRegions/Top/structure.py
@@ -0,0 +1,25 @@
+# structure configuration for datacard
+
+structure = {}
+
+# keys here must match keys in samples.py    
+structure['DY'] = {   
+    'isSignal' : 0,
+    'isData'   : 0 
+}
+
+structure['top'] = {   
+    'isSignal' : 0,
+    'isData'   : 0 
+}
+
+structure['WW']  = {
+    'isSignal' : 0,
+    'isData'   : 0    
+}
+
+# data
+structure['DATA']  = { 
+    'isSignal' : 0,
+    'isData'   : 1 
+}
