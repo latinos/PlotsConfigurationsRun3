40fa98c102a40e04c7bc2bf1b3a1a4524a8679c2
Sebastiano Quinto <squinto@lxplus990.cern.ch>
2025-03-13 13:20:42 +0100

    First commit of HWW config

---
+++ HWW/2022/structure.py
@@ -0,0 +1,120 @@
+# structure configuration for datacard
+
+structure = {}
+
+# scaleSampleForDatacard
+# keys here must match keys in samples.py    
+#                    
+
+structure['DY']  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0,
+                  # 'scaleSampleForDatacard' : 150/26.7
+              }
+
+structure['Fake_e']  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0,
+                  # 'scaleSampleForDatacard' : 150/26.7
+              }
+
+structure['Fake_m']  = {  
+                  'isSignal' : 0,
+                  'isData'   : 0,
+                  # 'scaleSampleForDatacard' : 150/26.7
+              }
+
+structure['top'] = {   
+                  'isSignal' : 0,
+                  'isData'   : 0,
+                  # 'scaleSampleForDatacard' : 150/26.7
+                  }
+
+
+structure['ggWW']  = {
+                  'isSignal' : 0,
+                  'isData'   : 0,
+                  # 'scaleSampleForDatacard' : 150/26.7  
+                  }
+
+structure['Wg']  = { 
+                  'isSignal' : 0,
+                  'isData'   : 0,
+                  # 'scaleSampleForDatacard' : 150/26.7 
+                  }
+
+structure['Vg']  = { 
+                  'isSignal' : 0,
+                  'isData'   : 0,
+                  # 'scaleSampleForDatacard' : 150/26.7
+                  }
+
+structure['VgS'] = { 
+                  'isSignal' : 0,
+                  'isData'   : 0,
+                  # 'scaleSampleForDatacard' : 150/26.7
+                  }
+
+structure['VgS_L'] = {
+                  'isSignal' : 0,
+                  'isData'   : 0,
+                  # 'scaleSampleForDatacard' : 150/26.7
+                  }
+
+structure['VgS_H'] = {
+                  'isSignal' : 0,
+                  'isData'   : 0,
+                  # 'scaleSampleForDatacard' : 150/26.7
+                  }
+
+structure['Zg']  = { 
+                  'isSignal' : 0,
+                  'isData'   : 0,
+                  # 'scaleSampleForDatacard' : 150/26.7
+                  }
+
+structure['VZ']  = { 
+                  'isSignal' : 0,
+                  'isData'   : 0,
+                  # 'scaleSampleForDatacard' : 150/26.7 
+                  }
+
+structure['WW']  = { 
+                  'isSignal' : 0,
+                  'isData'   : 0,
+                  # 'scaleSampleForDatacard' : 150/26.7 
+                  }
+
+
+structure['VVV']  = { 
+                  'isSignal' : 0,
+                  'isData'   : 0,
+                  # 'scaleSampleForDatacard' : 150/26.7 
+                  }
+
+structure['ZZ']  = {
+                  'isSignal' : 0,
+                  'isData'   : 0,
+                  # 'scaleSampleForDatacard' : 150/26.7    
+                  }
+
+structure['ggH_hww'] = {
+                  'isSignal' : 1,
+                  'isData'   : 0,
+                  # 'scaleSampleForDatacard' : 150/26.7    
+                  }
+
+structure['qqH_hww'] = {
+                  'isSignal' : 1,
+                  'isData'   : 0,
+                  # 'scaleSampleForDatacard' : 0.1*150/26.7    
+                  }
+
+# data
+
+
+structure['DATA']  = { 
+                  'isSignal' : 0,
+                  'isData'   : 1,
+                  # 'scaleSampleForDatacard' : 150/26.7
+              }
