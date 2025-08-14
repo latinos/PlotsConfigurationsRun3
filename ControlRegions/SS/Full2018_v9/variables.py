6131e87ff3d1d81ab2b11b0c57510783ca85f7d8
ntrevisa <nicolo.trevisani@cern.ch>
2025-08-14 08:40:37 +0200

    same-sign CR 2018

--- ControlRegions/SS/Full2018_v9/variables.py
+++ ControlRegions/SS/Full2018_v9/variables.py
@@ -15,25 +15,25 @@ variables['events'] = {
 # Control variables
 ###################
 
-variables['puppimet'] = {
-    'name'  : 'PuppiMET_pt',    
-    'range' : (20,0,200),
-    'xaxis' : 'PUPPI met [GeV]',
-    'fold'  : 3
+variables['BDTG6_TT'] = {
+    'name'     : 'BDT_WHSS_TopSemileptonic_v9',     
+    'range'    : (40,-1,1),   
+    'xaxis'    : 'BDT discriminant',
+    'fold'     : 3,
 }
 
-variables['mll'] = {
-    'name'  : 'mll',
-    'range' : (20, 40,120),
-    'xaxis' : 'm_{ll} [GeV]',
-    'fold'  : 0
+variables['BDT_WH3l_OSSF_new_v9'] = { 
+    'name'  : 'BDT_WH3l_OSSF_new_v9',
+    'range' : (40,-1.,1.),
+    'xaxis' : 'MVA discriminant',
+    'fold'  : 3,
 }
 
-variables['ptll'] = {
-    'name'  : 'ptll',     
-    'range' : (20, 0,200),   
-    'xaxis' : 'p_{T}^{ll} [GeV]',
-    'fold'  : 0
+variables['BDT_WH3l_SSSF_new_v9'] = { 
+    'name'  : 'BDT_WH3l_SSSF_new_v9',
+    'range' : (40,-1.,1.),
+    'xaxis' : 'MVA discriminant',
+    'fold'  : 3,
 }
 
 variables['pt1'] = {
@@ -45,57 +45,78 @@ variables['pt1'] = {
 
 variables['pt2'] = {
     'name'  : 'Lepton_pt[1]',     
-    'range' : (20,0,100),   
+    'range' : (30,0,100),   
     'xaxis' : 'p_{T} 2nd lep',
     'fold'  : 3                         
 }
 
-variables['mlljj20_whss_bin2'] = {
-    'name'  : 'mlljj20_whss',
-    # 'range' : ([60.,100.,110.,120.,130.,140.,150.,160.,170.,180.,190.,200.,250.,300.],),
-    'range' : (20,0,1000),
-    'xaxis' : 'mlljj20_whss [GeV]',
-    'fold'  : 3
+variables['conept1'] = {
+    'name'  : 'Lepton_conept[0]',     
+    'range' : (30,0,150),   
+    'xaxis' : 'p_{T} 1st lep',
+    'fold'  : 0                         
 }
 
-variables['detall']  = {  
-    'name'  : 'abs(Lepton_eta[0] - Lepton_eta[1])',
-    'range' : (20,0.,10.),
-    'xaxis' : 'detall',
-    'fold'  : 3
-}
-
-variables['mth']  = {  
-    'name'  : 'mth',
-    'range' : (40,0,200),
-    'xaxis' : 'm_{T}^{H} [GeV]',
-    'fold'  : 0
-}
-
-variables['dphill']  = {  
-    'name'  : 'abs(dphill)',     
-    'range' : (20,0,3.14),   
-    'xaxis' : '#Delta#phi_{ll}',
-    'fold'  : 3
+variables['conept2'] = {
+    'name'  : 'Lepton_conept[1]',     
+    'range' : (30,0,100),   
+    'xaxis' : 'p_{T} 2nd lep',
+    'fold'  : 3                         
 }
 
 variables['eta1']  = {
     'name'  : 'Lepton_eta[0]',     
-    'range' : (40,-3,3),   
+    'range' : (50,-2.5,2.5),   
     'xaxis' : '#eta 1st lep',
     'fold'  : 3                         
 }
 
 variables['eta2']  = {
     'name'  : 'Lepton_eta[1]',     
-    'range' : (40,-3,3),   
+    'range' : (50,-2.5,2.5),   
     'xaxis' : '#eta 2nd lep',
     'fold'  : 3                         
 }
 
+variables['detall']  = {  
+    'name'  : 'abs(Lepton_eta[0] - Lepton_eta[1])',
+    'range' : (20,0.,5.),
+    'xaxis' : '#Delta#eta_{jj}',
+    'fold'  : 3
+}
+
+variables['dphill']  = {  
+    'name'  : 'abs(dphill)',     
+    'range' : (20, 0,3.2),   
+    'xaxis' : '#Delta#phi_{ll}',
+    'fold'  : 3
+}
+
+variables['drll']  = {  
+    'name'  : 'abs(drll)',     
+    'range' : (20, 0,5.),   
+    'xaxis' : '#Delta#R_{ll}',
+    'fold'  : 3
+}
+
+variables['mll'] = {
+    'name'  : 'mll',
+    'range' : (25, 0,250),
+    'xaxis' : 'm_{ll} [GeV]',
+    'fold'  : 0
+}
+
+variables['ptll'] = {
+    'name'  : 'ptll',     
+    'range' : (20, 0,200),   
+    'xaxis' : 'p_{T}^{ll} [GeV]',
+    'fold'  : 0
+}
+
+
 variables['jetpt1']  = {
     'name'  : 'CleanJet_pt[0]*(CleanJet_pt[0]>30)',     
-    'range' : (40,0,200),   
+    'range' : (40,0,400),   
     'xaxis' : 'p_{T} 1st jet',
     'fold'  : 2   
 }
@@ -107,6 +128,34 @@ variables['jetpt2'] = {
     'fold'  : 0
 }
 
+variables['jeteta1']  = {
+    'name'  : 'CleanJet_eta[0]*(CleanJet_pt[0]>30)',     
+    'range' : (50,-5,5),   
+    'xaxis' : '#eta 1st jet',
+    'fold'  : 2   
+}
+
+variables['jeteta2'] = {
+    'name'  : 'CleanJet_eta[1]*(CleanJet_pt[1]>30)',     
+    'range' : (50,-5,5),   
+    'xaxis' : '#eta 2nd jet',
+    'fold'  : 0
+}
+
+variables['detajj']  = {  
+    'name'  : 'abs(CleanJet_eta[0] - CleanJet_eta[1])*(CleanJet_pt[1]>30)',
+    'range' : (20,0.,10.),
+    'xaxis' : '#Delta#eta_{jj}',
+    'fold'  : 3
+}
+
+variables['dphijj'] = {
+    'name'  : 'dphijj',
+    'range' : (20,0,3.2),
+    'xaxis' : '#Delta#phi_{jj}',
+    'fold'  : 3
+}
+
 variables['mjj'] = {
     'name'  : 'mjj*(CleanJet_pt[1]>30)',
     'range' : (20,0,1000),
@@ -114,16 +163,39 @@ variables['mjj'] = {
     'fold'  : 3
 }
 
+
+variables['puppimet'] = {
+    'name'  : 'PuppiMET_pt',    
+    'range' : (20,0,200),
+    'xaxis' : 'PUPPI met [GeV]',
+    'fold'  : 3
+}
+
+
+variables['mlljj20'] = {
+    'name'  : 'mlljj20_whss',
+    'range' : (20,0,1000),
+    'xaxis' : 'mlljj20 [GeV]',
+    'fold'  : 3
+}
+
+variables['mth']  = {  
+    'name'  : 'mth',
+    'range' : (50,0,250),
+    'xaxis' : 'm_{T}^{H} [GeV]',
+    'fold'  : 0
+}
+
 variables['mtw1'] = {
     'name'  : 'mtw1',
-    'range' : (40,0,200),
+    'range' : (50,0,250),
     'xaxis' : 'm_{T}^{W_{1}} [GeV]',
     'fold'  : 3
 }
 
 variables['mtw2'] = {
     'name'  : 'mtw2',
-    'range' : (40,0,200),
+    'range' : (50,0,200),
     'xaxis' : 'm_{T}^{W_{2}} [GeV]',
     'fold'  : 3
 }
@@ -200,15 +272,8 @@ variables['dphijjmet'] = {
 
 variables['ht'] = {
     'name'  : 'ht',
-    'range' : (40,0,200),
+    'range' : (30,0,1500),
     'xaxis' : 'ht [GeV]',
-    'fold'  : 3
-}
-
-variables['dphijj'] = {
-    'name'  : 'dphijj',
-    'range' : (20,0,3.2),
-    'xaxis' : 'dphijj',
     'fold'  : 3
 }
 
