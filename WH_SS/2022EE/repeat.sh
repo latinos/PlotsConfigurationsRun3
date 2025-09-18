2acf67a28f8c001242963a757ce8ae6af207e129
mwulansa <meutia.wulansatiti@cern.ch>
2025-09-18 14:42:47 +0200

    First commit for WH_SS

---
+++ WH_SS/2022EE/repeat.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+	echo "$0: Missing arguments"
+	exit 1
+else
+	echo "We got some argument(s)"
+	echo "==========================="
+	echo "Number of arguments. : $#"
+	echo "List of arguments... : $@"
+	echo "Arg #1: sample       : $1"
+	echo "Arg #2: compile      : $2"
+	echo "==========================="
+	SAMPLE=$1
+	COMPILE=$2
+fi
+
+
+if [ "$COMPILE" == "True" ]; then
+	mkShapesRDF -c 1
+	mkShapesRDF -o 0 -f . -b 1 -dR 1
+fi
+cd condor/WZ2022EE_v12/${SAMPLE}/
+cp ../../../../../../../../mkShapesRDF/mkShapesRDF/include/headers.hh ../../../../../../../../mkShapesRDF/mkShapesRDF/shapeAnalysis/runner.py  .
+python runner.py
+cp output.root /eos/user/n/ntrevisa/mkShapesRDF_rootfiles/WZ2022EE_v12/rootFile/mkShapes__WZ2022EE_v12__ALL__${SAMPLE}.root
+rm output.root
