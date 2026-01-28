7c08bfe40ad64447ec6b08664fe6d313e488f72a
Sebastiano Quinto <squinto@lxplus909.cern.ch>
2026-01-28 08:49:05 +0100

    SS CR for 2024

---
+++ ControlRegions/SS/2024_v15/nuisances.py
@@ -0,0 +1,70 @@
+mcProduction = 'Summer24_150x_nAODv15_Full2024v15'
+mcSteps      = 'MCl2loose2024v15__MCCorr2024v15__JERFrom23BPix__l2tight'
+dataRecoMuon     = 'Run2024_ReRecoCDE_PromptFGHI_nAODv15_Full2024v15_Muon'
+dataRecoEGamma     = 'Run2024_ReRecoCDE_PromptFGHI_nAODv15_Full2024v15_EGamma'
+dataRecoMuonEG     = 'Run2024_ReRecoCDE_PromptFGHI_nAODv15_Full2024v15_MuonEG'
+dataSteps    = 'DATAl2loose2024v15__l2loose'
+
+treeBaseDir = '/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano'
+limitFiles = -1
+
+mc = [skey for skey in samples if skey not in ('Fake', 'DATA')]
+
+redirector = ""
+
+useXROOTD = False
+
+def makeMCDirectory(var=''):
+    _treeBaseDir = treeBaseDir + ''
+    if useXROOTD:
+        _treeBaseDir = redirector + treeBaseDir
+    if var== '':
+        return '/'.join([_treeBaseDir, mcProduction, mcSteps])
+    else:
+        return '/'.join([_treeBaseDir, mcProduction, mcSteps + '__' + var])
+
+
+
+mcDirectory = makeMCDirectory()
+#fakeDirectory = os.path.join(treeBaseDir, dataReco, fakeSteps)
+#dataDirectory = os.path.join(treeBaseDir, dataReco, dataSteps)
+print(treeBaseDir)
+
+# merge cuts
+_mergedCuts = []
+for cut in list(cuts.keys()):
+    __cutExpr = ''
+    if type(cuts[cut]) == dict:
+        __cutExpr = cuts[cut]['expr']
+        for cat in list(cuts[cut]['categories'].keys()):
+            _mergedCuts.append(cut + '_' + cat)
+    elif type(cuts[cut]) == str:
+        _mergedCuts.append(cut)
+
+cuts2j = _mergedCuts
+
+nuisances = {}
+
+
+################################ EXPERIMENTAL UNCERTAINTIES  #################################
+
+
+nuisances['fake_syst'] = {
+    'name': 'CMS_fake_syst',
+    'type': 'lnN',
+    'samples': {
+        'Fake': '1.3'
+    },
+}
+
+autoStats = True
+if autoStats:
+    ## Use the following if you want to apply the automatic combine MC stat nuisances.
+    nuisances['stat'] = {
+        'type': 'auto',
+        'maxPoiss': '10',
+        'includeSignal': '0',
+        #  nuisance ['maxPoiss'] =  Number of threshold events for Poisson modelling
+        #  nuisance ['includeSignal'] =  Include MC stat nuisances on signal processes (1=True, 0=False)
+        'samples': {}
+    }
