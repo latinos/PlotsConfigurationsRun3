7f2cc3d836b07e16ac90a2cfd15e42a2145f479a
ntrevisa <nicolo.trevisani@cern.ch>
2026-01-30 13:06:41 +0100

    coherent pt thresholds and names

--- FakeRate/2024_v15/do_fake_rate.sh
+++ FakeRate/2024_v15/do_fake_rate.sh
@@ -1,5 +1,3 @@
-cd ../scripts/
-
 if [[ "$(uname -a)" == *portal* ]] || [[ "$(uname -a)" == *bms* ]]; then
     echo "We are at KIT!"
     input_dir=/ceph/${USER}
@@ -8,28 +6,47 @@ else
     input_dir=/eos/user/${USER:0:1}/${USER}
 fi
 
-# Muons
-python mkFakeRate.py --inputFile ${input_dir}/rootFiles/FakeRate/2024v15/rootFiles__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67/mkShapes__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67.root --outputFolder /eos/user/s/squinto/FakeRate/2024_v15_conept/cut_TightID_pfIsoTight_HWW_tthmva_67 --outputFileName MuonFR_jet45.root --jet_pt 45 --flavor muon --variable conept2_eta2 --do_prompt_rate True --outputFileNamePR MuonPR.root
 
-python mkFakeRate.py --inputFile ${input_dir}/rootFiles/FakeRate/2024v15/rootFiles__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67/mkShapes__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67.root --outputFolder /eos/user/s/squinto/FakeRate/2024_v15_conept/cut_TightID_pfIsoTight_HWW_tthmva_67 --outputFileName MuonFR_jet10.root --jet_pt 10 --flavor muon --variable conept1_eta1
-python mkFakeRate.py --inputFile ${input_dir}/rootFiles/FakeRate/2024v15/rootFiles__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67/mkShapes__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67.root --outputFolder /eos/user/s/squinto/FakeRate/2024_v15_conept/cut_TightID_pfIsoTight_HWW_tthmva_67 --outputFileName MuonFR_jet15.root --jet_pt 15 --flavor muon --variable conept1_eta1
-python mkFakeRate.py --inputFile ${input_dir}/rootFiles/FakeRate/2024v15/rootFiles__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67/mkShapes__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67.root --outputFolder /eos/user/s/squinto/FakeRate/2024_v15_conept/cut_TightID_pfIsoTight_HWW_tthmva_67 --outputFileName MuonFR_jet20.root --jet_pt 20 --flavor muon --variable conept1_eta1
-python mkFakeRate.py --inputFile ${input_dir}/rootFiles/FakeRate/2024v15/rootFiles__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67/mkShapes__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67.root --outputFolder /eos/user/s/squinto/FakeRate/2024_v15_conept/cut_TightID_pfIsoTight_HWW_tthmva_67 --outputFileName MuonFR_jet25.root --jet_pt 25 --flavor muon --variable conept1_eta1
-python mkFakeRate.py --inputFile ${input_dir}/rootFiles/FakeRate/2024v15/rootFiles__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67/mkShapes__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67.root --outputFolder /eos/user/s/squinto/FakeRate/2024_v15_conept/cut_TightID_pfIsoTight_HWW_tthmva_67 --outputFileName MuonFR_jet30.root --jet_pt 30 --flavor muon --variable conept1_eta1
-python mkFakeRate.py --inputFile ${input_dir}/rootFiles/FakeRate/2024v15/rootFiles__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67/mkShapes__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67.root --outputFolder /eos/user/s/squinto/FakeRate/2024_v15_conept/cut_TightID_pfIsoTight_HWW_tthmva_67 --outputFileName MuonFR_jet35.root --jet_pt 35 --flavor muon --variable conept1_eta1
-python mkFakeRate.py --inputFile ${input_dir}/rootFiles/FakeRate/2024v15/rootFiles__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67/mkShapes__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67.root --outputFolder /eos/user/s/squinto/FakeRate/2024_v15_conept/cut_TightID_pfIsoTight_HWW_tthmva_67 --outputFileName MuonFR_jet40.root --jet_pt 40 --flavor muon --variable conept1_eta1
-python mkFakeRate.py --inputFile ${input_dir}/rootFiles/FakeRate/2024v15/rootFiles__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67/mkShapes__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67.root --outputFolder /eos/user/s/squinto/FakeRate/2024_v15_conept/cut_TightID_pfIsoTight_HWW_tthmva_67 --outputFileName MuonFR_jet45.root --jet_pt 45 --flavor muon --variable conept1_eta1
+cd ../scripts/
+
+
+MUON_ID=cut_TightID_pfIsoTight_HWW_tthmva_67
+ELE_ID=cutBased_LooseID_tthMVA_Run3
+
+
+### Cone pT
+
+# Muons
+python mkFakeRate.py --inputFile ${input_dir}/mkShapesRDF_rootfiles/2024v15_${ELE_ID}__mu_${MUON_ID}/rootFile/mkShapes__2024v15_${ELE_ID}__mu_${MUON_ID}.root --outputFolder ../2024_v15/FakeRate_cone_pt/2024_v15_conept/${MUON_ID} --outputFileName MuonFR_jet30.root --jet_pt 30 --flavor muon --variable conept2_eta2 --do_prompt_rate True --outputFileNamePR MuonPR.root
+
+python mkFakeRate.py --inputFile ${input_dir}/mkShapesRDF_rootfiles/2024v15_${ELE_ID}__mu_${MUON_ID}/rootFile/mkShapes__2024v15_${ELE_ID}__mu_${MUON_ID}.root --outputFolder ../2024_v15/FakeRate_cone_pt/2024_v15_conept/${MUON_ID} --outputFileName MuonFR_jet20.root --jet_pt 20 --flavor muon --variable conept1_eta1
+python mkFakeRate.py --inputFile ${input_dir}/mkShapesRDF_rootfiles/2024v15_${ELE_ID}__mu_${MUON_ID}/rootFile/mkShapes__2024v15_${ELE_ID}__mu_${MUON_ID}.root --outputFolder ../2024_v15/FakeRate_cone_pt/2024_v15_conept/${MUON_ID} --outputFileName MuonFR_jet30.root --jet_pt 30 --flavor muon --variable conept1_eta1
+python mkFakeRate.py --inputFile ${input_dir}/mkShapesRDF_rootfiles/2024v15_${ELE_ID}__mu_${MUON_ID}/rootFile/mkShapes__2024v15_${ELE_ID}__mu_${MUON_ID}.root --outputFolder ../2024_v15/FakeRate_cone_pt/2024_v15_conept/${MUON_ID} --outputFileName MuonFR_jet40.root --jet_pt 40 --flavor muon --variable conept1_eta1
+
 
 # Electrons
-python mkFakeRate.py --inputFile ${input_dir}/rootFiles/FakeRate/2024v15/rootFiles__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67/mkShapes__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67.root --outputFolder /eos/user/s/squinto/FakeRate/2024_v15_conept/cutBased_LooseID_tthMVA_Run3 --outputFileName EleFR_jet45.root --jet_pt 45 --flavor ele --variable conept2_eta2 --do_prompt_rate True --outputFileNamePR ElePR.root
+python mkFakeRate.py --inputFile ${input_dir}/mkShapesRDF_rootfiles/2024v15_${ELE_ID}__mu_${MUON_ID}/rootFile/mkShapes__2024v15_${ELE_ID}__mu_${MUON_ID}.root --outputFolder ../2024_v15/FakeRate_cone_pt/2024_v15_conept/${ELE_ID} --outputFileName EleFR_jet30.root --jet_pt 30 --flavor ele --variable conept2_eta2 --do_prompt_rate True --outputFileNamePR ElePR.root
 
-python mkFakeRate.py --inputFile ${input_dir}/rootFiles/FakeRate/2024v15/rootFiles__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67/mkShapes__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67.root --outputFolder /eos/user/s/squinto/FakeRate/2024_v15_conept/cutBased_LooseID_tthMVA_Run3 --outputFileName EleFR_jet10.root --jet_pt 10 --flavor ele --variable conept1_eta1
-python mkFakeRate.py --inputFile ${input_dir}/rootFiles/FakeRate/2024v15/rootFiles__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67/mkShapes__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67.root --outputFolder /eos/user/s/squinto/FakeRate/2024_v15_conept/cutBased_LooseID_tthMVA_Run3 --outputFileName EleFR_jet15.root --jet_pt 15 --flavor ele --variable conept1_eta1
-python mkFakeRate.py --inputFile ${input_dir}/rootFiles/FakeRate/2024v15/rootFiles__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67/mkShapes__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67.root --outputFolder /eos/user/s/squinto/FakeRate/2024_v15_conept/cutBased_LooseID_tthMVA_Run3 --outputFileName EleFR_jet20.root --jet_pt 20 --flavor ele --variable conept1_eta1
-python mkFakeRate.py --inputFile ${input_dir}/rootFiles/FakeRate/2024v15/rootFiles__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67/mkShapes__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67.root --outputFolder /eos/user/s/squinto/FakeRate/2024_v15_conept/cutBased_LooseID_tthMVA_Run3 --outputFileName EleFR_jet25.root --jet_pt 25 --flavor ele --variable conept1_eta1
-python mkFakeRate.py --inputFile ${input_dir}/rootFiles/FakeRate/2024v15/rootFiles__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67/mkShapes__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67.root --outputFolder /eos/user/s/squinto/FakeRate/2024_v15_conept/cutBased_LooseID_tthMVA_Run3 --outputFileName EleFR_jet30.root --jet_pt 30 --flavor ele --variable conept1_eta1
-python mkFakeRate.py --inputFile ${input_dir}/rootFiles/FakeRate/2024v15/rootFiles__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67/mkShapes__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67.root --outputFolder /eos/user/s/squinto/FakeRate/2024_v15_conept/cutBased_LooseID_tthMVA_Run3 --outputFileName EleFR_jet35.root --jet_pt 35 --flavor ele --variable conept1_eta1
-python mkFakeRate.py --inputFile ${input_dir}/rootFiles/FakeRate/2024v15/rootFiles__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67/mkShapes__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67.root --outputFolder /eos/user/s/squinto/FakeRate/2024_v15_conept/cutBased_LooseID_tthMVA_Run3 --outputFileName EleFR_jet40.root --jet_pt 40 --flavor ele --variable conept1_eta1
-python mkFakeRate.py --inputFile ${input_dir}/rootFiles/FakeRate/2024v15/rootFiles__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67/mkShapes__2024v15_cutBased_LooseID_tthMVA_Run3__mu_cut_TightID_pfIsoTight_HWW_tthmva_67.root --outputFolder /eos/user/s/squinto/FakeRate/2024_v15_conept/cutBased_LooseID_tthMVA_Run3 --outputFileName EleFR_jet45.root --jet_pt 45 --flavor ele --variable conept1_eta1
+python mkFakeRate.py --inputFile ${input_dir}/mkShapesRDF_rootfiles/2024v15_${ELE_ID}__mu_${MUON_ID}/rootFile/mkShapes__2024v15_${ELE_ID}__mu_${MUON_ID}.root --outputFolder ../2024_v15/FakeRate_cone_pt/2024_v15_conept/${ELE_ID} --outputFileName EleFR_jet20.root --jet_pt 20 --flavor ele --variable conept1_eta1
+python mkFakeRate.py --inputFile ${input_dir}/mkShapesRDF_rootfiles/2024v15_${ELE_ID}__mu_${MUON_ID}/rootFile/mkShapes__2024v15_${ELE_ID}__mu_${MUON_ID}.root --outputFolder ../2024_v15/FakeRate_cone_pt/2024_v15_conept/${ELE_ID} --outputFileName EleFR_jet30.root --jet_pt 30 --flavor ele --variable conept1_eta1
+python mkFakeRate.py --inputFile ${input_dir}/mkShapesRDF_rootfiles/2024v15_${ELE_ID}__mu_${MUON_ID}/rootFile/mkShapes__2024v15_${ELE_ID}__mu_${MUON_ID}.root --outputFolder ../2024_v15/FakeRate_cone_pt/2024_v15_conept/${ELE_ID} --outputFileName EleFR_jet40.root --jet_pt 40 --flavor ele --variable conept1_eta1
+
+
+### Lepton pT
+
+# Muons
+python mkFakeRate.py --inputFile ${input_dir}/mkShapesRDF_rootfiles/2024v15_${ELE_ID}__mu_${MUON_ID}/rootFile/mkShapes__2024v15_${ELE_ID}__mu_${MUON_ID}.root --outputFolder ../2024_v15/FakeRate_pt/2024_v15_pt/${MUON_ID} --outputFileName MuonFR_jet30.root --jet_pt 30 --flavor muon --variable pt2_eta2 --do_prompt_rate True --outputFileNamePR MuonPR.root
+
+python mkFakeRate.py --inputFile ${input_dir}/mkShapesRDF_rootfiles/2024v15_${ELE_ID}__mu_${MUON_ID}/rootFile/mkShapes__2024v15_${ELE_ID}__mu_${MUON_ID}.root --outputFolder ../2024_v15/FakeRate_pt/2024_v15_pt/${MUON_ID} --outputFileName MuonFR_jet20.root --jet_pt 20 --flavor muon --variable pt1_eta1
+python mkFakeRate.py --inputFile ${input_dir}/mkShapesRDF_rootfiles/2024v15_${ELE_ID}__mu_${MUON_ID}/rootFile/mkShapes__2024v15_${ELE_ID}__mu_${MUON_ID}.root --outputFolder ../2024_v15/FakeRate_pt/2024_v15_pt/${MUON_ID} --outputFileName MuonFR_jet30.root --jet_pt 30 --flavor muon --variable pt1_eta1
+python mkFakeRate.py --inputFile ${input_dir}/mkShapesRDF_rootfiles/2024v15_${ELE_ID}__mu_${MUON_ID}/rootFile/mkShapes__2024v15_${ELE_ID}__mu_${MUON_ID}.root --outputFolder ../2024_v15/FakeRate_pt/2024_v15_pt/${MUON_ID} --outputFileName MuonFR_jet40.root --jet_pt 40 --flavor muon --variable pt1_eta1
+
+
+# Electrons
+python mkFakeRate.py --inputFile ${input_dir}/mkShapesRDF_rootfiles/2024v15_${ELE_ID}__mu_${MUON_ID}/rootFile/mkShapes__2024v15_${ELE_ID}__mu_${MUON_ID}.root --outputFolder ../2024_v15/FakeRate_pt/2024_v15_pt/${ELE_ID} --outputFileName EleFR_jet30.root --jet_pt 30 --flavor ele --variable pt2_eta2 --do_prompt_rate True --outputFileNamePR ElePR.root
+
+python mkFakeRate.py --inputFile ${input_dir}/mkShapesRDF_rootfiles/2024v15_${ELE_ID}__mu_${MUON_ID}/rootFile/mkShapes__2024v15_${ELE_ID}__mu_${MUON_ID}.root --outputFolder ../2024_v15/FakeRate_pt/2024_v15_pt/${ELE_ID} --outputFileName EleFR_jet20.root --jet_pt 20 --flavor ele --variable pt1_eta1
+python mkFakeRate.py --inputFile ${input_dir}/mkShapesRDF_rootfiles/2024v15_${ELE_ID}__mu_${MUON_ID}/rootFile/mkShapes__2024v15_${ELE_ID}__mu_${MUON_ID}.root --outputFolder ../2024_v15/FakeRate_pt/2024_v15_pt/${ELE_ID} --outputFileName EleFR_jet30.root --jet_pt 30 --flavor ele --variable pt1_eta1
+python mkFakeRate.py --inputFile ${input_dir}/mkShapesRDF_rootfiles/2024v15_${ELE_ID}__mu_${MUON_ID}/rootFile/mkShapes__2024v15_${ELE_ID}__mu_${MUON_ID}.root --outputFolder ../2024_v15/FakeRate_pt/2024_v15_pt/${ELE_ID} --outputFileName EleFR_jet40.root --jet_pt 40 --flavor ele --variable pt1_eta1
 
 cd -
